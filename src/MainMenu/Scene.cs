b990b4f6a0e61f076e5e3e296c934cab4e0e0961
Luca Halm <luca.halm@lhsoftware.ch>
2024-03-20 16:21:59 +0100

    Minor start screen improvement

--- src/MainMenu/Scene.cs
+++ src/MainMenu/Scene.cs
@@ -5,14 +5,17 @@ using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TinyShopping.MainMenu
 {
-    internal class Scene : global::TinyShopping.Scene {
+    internal class Scene : TinyShopping.Scene {
 
         private Texture2D _background;
+
+        private SpriteFont _font;
 
         private Vector2 _offset;
 
@@ -24,13 +27,15 @@ namespace TinyShopping.MainMenu
 
         public override void LoadContent() {
             _background = Content.Load<Texture2D>("teaser");
+            _font = Content.Load<SpriteFont>("Arial");
             CalculateBackgroundPosition();
             base.LoadContent();
         }
 
         public override void Update(GameTime gameTime) {
             KeyboardState state = Keyboard.GetState();
-            if (state.GetPressedKeys().Length > 0) {
+            GamePadState cState = GamePad.GetState(PlayerIndex.One);
+            if (state.GetPressedKeys().Length > 0 || cState.IsButtonDown(Buttons.A)) {
                 Game.ChangeScene(new Game.Scene(Content, GraphicsDevice, GraphicsDeviceManager, Game));
             }
             base.Update(gameTime);
@@ -39,6 +44,11 @@ namespace TinyShopping.MainMenu
         public override void Draw(GameTime gameTime) {
             SpriteBatch.Begin();
             SpriteBatch.Draw(_background, _backgroundPosition, Color.White);
+            Vector2 pos = new Vector2(GraphicsDeviceManager.PreferredBackBufferWidth/2, GraphicsDeviceManager.PreferredBackBufferHeight*5/6);
+            String message = "Press any key to continue";
+            Vector2 textSize = _font.MeasureString(message);
+            SpriteBatch.DrawString(_font, message, pos, Color.Black, 0, textSize / 2, 2.01f, SpriteEffects.None, 0);
+            SpriteBatch.DrawString(_font, message, pos, Color.White, 0, textSize/2, 2, SpriteEffects.None, 0);
             SpriteBatch.End();
             base.Draw(gameTime);
         }
@@ -50,10 +60,10 @@ namespace TinyShopping.MainMenu
             Vector2 screen = new Vector2(GraphicsDeviceManager.PreferredBackBufferWidth, GraphicsDeviceManager.PreferredBackBufferHeight);
             float ratio;
             if (_background.Height / screen.Y > _background.Width / screen.X) {
-                ratio = screen.Y / (float)_background.Height;
+                ratio = screen.Y / _background.Height;
             }
             else {
-                ratio = screen.X / (float)_background.Width;
+                ratio = screen.X / _background.Width;
             }
             int worldWidth = (int)(_background.Width * ratio);
             int worldHeight = (int)(_background.Height * ratio);
