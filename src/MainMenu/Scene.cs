41061f4bfde4ab320028a5fdcb028854f2c3c005
Claudio Abart <claudio.abart@gmail.com>
2024-04-14 22:10:45 +0200

    Cleanup imports

--- src/MainMenu/Scene.cs
+++ src/MainMenu/Scene.cs
@@ -1,5 +1,4 @@
-﻿using System.Buffers;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 
--- src/StartupScreen/Scene.cs
+++ src/StartupScreen/Scene.cs
@@ -2,7 +2,6 @@
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using System;
 
 namespace TinyShopping.StartupScreen
 {
